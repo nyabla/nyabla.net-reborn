07e7b99bc83bb372518ed13f5f5d671ce3eef029
nela <16510465+nyabla@users.noreply.github.com>
2023-07-28 08:55:58 +0000

    .eleventy.js: add `date` shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,13 @@ module.exports = function(eleventyConfig) {
     // copy `static/` to `_site/`
     eleventyConfig.addPassthroughCopy({ "src/static": "/" })
 
+    // date shortcode
+    eleventyConfig.addShortcode("date", (date) => {
+        const formatted = date.toISOString().slice(0, 10)
+        const content = `<time datetime="${formatted}">${formatted}</time>`
+        return content
+    })
+
     // input folder `src/`
     return {
         dir: {
