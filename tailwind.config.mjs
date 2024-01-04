658b9ecaf80cd3b145d01c66ed8171898e7c562a
nyabla <16510465+nyabla@users.noreply.github.com>
2024-01-04 20:31:29 +0000

    tailwind config: use spaces to indent

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,27 +2,27 @@ const stack = "Inter, Roboto, 'Helvetica Neue', 'Arial Nova', 'Nimbus Sans', Ari
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: [
-					stack,
-					{
-						fontFeatureSettings: '"zero" on, "cv01" on, "cv05" on',
-				  	},
-				],
-				display: [
-					`"Inter Display", ${stack}`,
-					{
-						fontFeatureSettings: '"zero" on, "cv01" on, "cv05" on',
-					}
-				],
-			},
-			colors: {
-				"nyabla-pink": "#D40055",
-			}
-		},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          stack,
+          {
+            fontFeatureSettings: '"zero" on, "cv01" on, "cv05" on',
+            },
+        ],
+        display: [
+          `"Inter Display", ${stack}`,
+          {
+            fontFeatureSettings: '"zero" on, "cv01" on, "cv05" on',
+          }
+        ],
+      },
+      colors: {
+        "nyabla-pink": "#D40055",
+      }
+    },
+  },
+  plugins: [],
 }
